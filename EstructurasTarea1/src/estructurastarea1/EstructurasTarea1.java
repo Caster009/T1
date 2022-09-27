21baeb07f9d09b4a961b0196a747092ace67e482
Caster009 <114118525+Caster009@users.noreply.github.com>
2022-09-27 17:28:59 -0600

    Add files via upload

---
+++ EstructurasTarea1/src/estructurastarea1/EstructurasTarea1.java
@@ -0,0 +1,21 @@
+package estructurastarea1;
+public class EstructurasTarea1 {
+int digito;
+int matriz;
+       
+    public static void main(String[] args) {
+        int digito;
+        int matriz;
+        
+        
+        
+        String[] prueba = new prueba String[]("Ronny", "Mark");
+            int [] matriz = new int [3] [3];
+            int cont=1;
+            for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+            matriz[i][j] =cont++;
+            }
+            }
+            }
+            }
